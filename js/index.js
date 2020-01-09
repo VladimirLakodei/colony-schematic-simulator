b481558acf1a0f553a8245c057192d5dd70cc961
Vova <vovalak@mail.ru>
2020-01-09 21:35:44 +0200

    base structure

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import Model from './model.js';
+import View from './view.js';
+import Controller from './controller.js';
+
+const root = document.querySelector('#game');
+
+const model = new Model();
+const view = new View(root);
+const controller = new Controller(game, model);
